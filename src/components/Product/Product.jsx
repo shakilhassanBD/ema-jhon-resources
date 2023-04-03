18ae2071a86a81f1c92390f479e503571579c0ef
shakilhassanBD <hasanshakil564@gmail.com>
2023-04-04 01:49:26 +0600

    first commit

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Product.css';
+const Product = (props) => {
+    const { name, seller, ratings, price, category, img } = props.product;
+    console.log(props.product);
+    return (
+        <div className='product'>
+            <div className='product-info'>
+                <img src={img} alt="image is not found" />
+                <h6 className='product-name'>{name}</h6>
+                <h6>Manufacturer: {seller}</h6>
+                <h6>Price: ${price}</h6>
+                <h6>Ratings: {ratings} stars</h6>
+            </div>
+            <button className='cart-btn'>Add to cart</button>
+        </div>
+
+    );
+};
+
+export default Product;
